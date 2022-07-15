f374fad2f2d3912800c7003958b3bc2d32eb5346
blrk <blradhakrishnan@gmail.com>
2022-07-15 10:04:17 +0530

    Create DatatypeVarScanner.java

---
+++ DatatypeVarScanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class DatatypeVarScanner {
+	public static void main(String s[]) {
+		int x;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter va value:");
+		x = sc.nextInt();
+		System.out.println("Value of x is:" + x);
+	}
+}
