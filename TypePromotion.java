af69c151d0d8db06efcde49cf249e5d7ecef6aef
blrk <blradhakrishnan@gmail.com>
2022-07-20 12:24:48 +0530

    Add files via upload

--- TypePromotion.java
+++ TypePromotion.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class TypePromotion {
 	public static void main(String[] args) {
 		byte a = 40, b = 50, c = 100;
 		int d = a * b / c ; 
+		//short d = a * b / c ; 
 		System.out.println("d:" + d);
+		System.out.println("a * b:" +  a * b);
+		//byte e = 2000;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		String s1 = sc.next();
+		int x = Integer.parseInt(s1);
+		System.out.println("x:" + x);
 	}
 }
